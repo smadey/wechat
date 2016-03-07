30ee73815b329e8f915aad71429c94db6a138456
Smadey <smadey@163.com>
2016-03-07 17:22:11 +0800

    调整消息解析功能

--- app.js
+++ app.js
@@ -11,32 +11,115 @@ var wechatConfig = {
   token: config.token,
   encodingAESKey: config.encodingAESKey
 };
-var REG_QUERY = /^cx:(.+)/;
+
+// 事件消息解析规则
+var events = [
+  {
+    type: 'subscribe',
+    handler: function (username, context) {
+      console.log('用户"' + username + '"订阅了！');
+
+      var textTips = texts.map(function (d, i) {
+        return (i + 1) + '.' + d.title + '\n' + d.content;
+      }).join('\n\n');
+
+      return '感谢您关注公众号！\n我们将为您提供以下服务\n\n' + textTips;
+    }
+  },
+  {
+    type: 'unsub',
+    handler: function (username, context) {
+      console.log('用户' + username + '取消订阅了！');
+    }
+  }
+];
+
+// 文本消息解析规则
+var texts = [
+  {
+    title: '测试',
+    content: '提交“测试：n”返回第n条中文测试结果，如“测试：2”返回第2条中文测试结果',
+    reg: /^测试：(.+)/,
+    handler: function (text) {
+      return '这是第' + text + '条测试结果';
+    }
+  },
+  {
+    title: 'test',
+    content: '提交“test:n”返回第n条英文测试结果，如“test:2”返回第3条英文测试结果',
+    reg: /^test:(.+)/,
+    handler: function (text) {
+      return 'This the ' + text + ' test result';
+    }
+  }
+];
 
 app.set('port', port);
 app.use(express.query());
 app.use('/msg',
-  wechat(wechatConfig, function(req, res, next) {
+  wechat(wechatConfig, function(req, res) {
     var msg = req.weixin;
-    var match
+    var i;
+    var n;
 
-    console.log(req);
+    switch (msg.MsgType) {
+      case 'event':
+        // 解析用户触发事件
+        console.log('用户“' + msg.FromUserName + '”触发了“' + msg.Event + '”事件');
 
-    if (match = query(msg, REG_QUERY)) {
-      res.reply('您查询的“' + match + '”的库存为：0');
-    } else {
-      next();
+        var evt;
+
+        for (i = 0, n = events.length; i < n; i++) {
+          evt = events[i];
+
+          if (msg.Event === evt.type) {
+            res.reply(evt.handler(msg.FromUserName, msg));
+            break;
+          }
+        }
+
+        break;
+
+      case 'text':
+        // 解析用户文本输入
+        console.log('用户“' + msg.FromUserName + '”输入了“' + msg.Content + '”');
+
+        var text;
+        var query;
+
+        for (i = 0, n = texts.length; i < n; i++) {
+          text = texts[i];
+
+          if (query = match(msg.Content, text.reg)) {
+            res.reply(text.handler(query));
+            text = null;
+            break;
+          }
+        }
+
+        if (text) {
+          var textTips = texts.map(function (d, i) {
+            return (i + 1) + '.' + d.title + '\n' + d.content;
+          }).join('\n');
+
+          res.reply('仅支持以下功能：\n' + textTips + '\n\n请重新输入！');
+        }
+
+        break;
+
+      default:
+        res.reply('当前版本只支持文本查询功能！更多更能，敬请期待。。。');
     }
   })
 )
 
 app.listen(port, function () {
   console.log('Express server listening on port ' + port);
-})
+});
 
 module.exports = app;
 
-function query(str, reg) {
+function match(str, reg) {
   var result = str.match(reg)
 
   if (result) {
